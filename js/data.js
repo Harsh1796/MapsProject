1f6218c90849b68a85155845bd18a0d4b28d0018
sivcan.singh <sivcan@getpostman.com>
2018-04-17 19:13:22 +0530

    Updated CSS in sidebar

---
+++ js/data.js
@@ -0,0 +1,108 @@
+var initialSpaces = [
+{
+  "name": "Jungle Cafe",
+  "location": {"lat": 28.5010413, "lng": 77.1855449},
+  "res_id": "18232132"
+},
+{
+  "name": "Glued Entertainment",
+  "location": {"lat": 28.5614733, "lng": 77.3607146},
+  "res_id": "8977"
+},
+{
+  "name": "Taj Cafe Coffee Day",
+  "location": {"lat": 28.5951278963, "lng": 77.1719860286},
+  "res_id": "600",
+  "shortUrl": "https://b.zmtcdn.com/data/pictures/0/600/5dba72f2db93f57096549a9958a59ac9.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"
+},
+{
+  "name": "The Forestta",
+  "location": {"lat": 28.4392450000, "lng": 77.1039910000},
+  "res_id": "18594230"
+},
+{
+  "name": "RBG - Park Inn by Radisson",
+  "location": {"lat": 28.6187263098, "lng": 77.2873790190},
+  "res_id": "310379"
+},
+{
+  "name": "Throttle Shrottle",
+  "location": {"lat": 28.4281428920, "lng": 77.1427831426},
+  "res_id": "311615"
+},
+{
+  "name": "Kitchen Cafe",
+  "location": {"lat": 28.6410129514, "lng": 77.2130027041},
+  "res_id": "310444"
+},
+{
+  "name": "Penta Cafe",
+  "location": {"lat": 28.6992230326, "lng": 77.1916197985},
+  "res_id": "18034044"
+},
+{
+  "name": "Thok (The House of Kakori)",
+  "location": {"lat": 28.5616244277, "lng": 77.2496141866},
+  "res_id": "18252362"
+},
+{
+  "name": "Sandwhich King",
+  "location": {"lat": 28.6139471261, "lng": 77.3612762243},
+  "res_id": "3939"
+},
+{
+  "name": "All Time Cafe",
+  "location": {"lat": 28.4203344709, "lng": 77.0510626584},
+  "res_id": "18450874"
+},
+{
+  "name": "Moonlight Adda",
+  "location": {"lat": 28.4611250000, "lng": 77.0792220000},
+  "res_id": "18618271",
+  "shortUrl": "https://b.zmtcdn.com/data/reviews_photos/18b/9376e26af795a44fa5a2d40df47cb18b_1513348983.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"
+},
+{
+  "name": "Horn Please",
+  "location": {"lat": 28.5374450285, "lng": 77.2565295920},
+  "res_id": "17953899"
+},
+{
+  "name": "Mukesh Dhaba",
+  "location": {"lat": 28.4935634000, "lng": 77.0858484000},
+  "res_id": "312266",
+  "shortUrl": "https://b.zmtcdn.com/data/pictures/6/312266/c46a8c46cdf026f23d4a77e2ac20e21f.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"
+},
+{
+  "name": "Spooky Sky",
+  "location": {"lat": 28.6403314621, "lng": 77.2106943280},
+  "res_id": "313371",
+  "shortUrl": "https://b.zmtcdn.com/data/pictures/1/313371/64675cd6b6dbac99992a7870157b8883.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"
+},
+{
+  "name": "The Millionare Express",
+  "location": {"lat": 28.4255028008, "lng": 77.0573215932},
+  "res_id": "18429397"
+},
+{
+  "name": "Billu's Hut Night Express",
+  "location": {"lat": 28.6947748518, "lng": 77.1533560753},
+  "res_id": "18633190",
+  "shortUrl": "https://b.zmtcdn.com/data/pictures/0/18633190/ba392a7a5c652b4ad621f18a1a9143de.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"
+},
+{
+  "name": "Cafe 24 - Hotel City Park",
+  "location": {"lat": 28.7059030363, "lng": 77.1432612464},
+  "res_id": "3951"
+},
+{
+  "name": "Pakode's Ki Dukaan",
+  "location": {"lat": 28.6530768745, "lng": 77.1943600103},
+  "res_id": "306071",
+  "shortUrl": "https://b.zmtcdn.com/data/pictures/1/306071/bd78e6c6b8f91013d85b16c5207c6a01.jpg?fit=around%7C200%3A2007&crop=200%3A200%3B%2A%2C%2A"
+},
+{
+  "name": "Yellow Brick Road - Taj Vivanta",
+  "location": {"lat": 28.6012063809, "lng": 77.2294463217},
+  "res_id": "4404"
+},
+]
